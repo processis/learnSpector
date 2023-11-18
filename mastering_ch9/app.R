bb15ed78009104ae1fbb629e7cf81257ff574a98
Debian Live user <edmondsksung@hotmail.com>
2023-11-18 15:13:57 +0800

    from prev session??

--- mastering_ch9/app.R
+++ mastering_ch9/app.R
@@ -1,23 +1,70 @@
+#Mastering ch 9 case study p150
 server <- function (input,output,
 session){
-  data <- reactive({
+  # upload -------
+  raw <- reactive({
     req(input$file)
-    
-    ext <- tools::file_ext(input$file$name)
-    switch(ext,
-        csv = vroom::vroom(input$file$datapath, delim = ","),
-        tsv = vroom::vroom(input$file$datapath, delim = "\t"),
-        validate("Invalid file; please upload a .csv or .tsv file")
-     )
+    delim <- if(input$delim == "") NULL else input$delim
+    vroom::vroom(input$file$datapath, delim = delim, skip = input$skip)
   })
-  
-  output$head <- renderTable({
-    head(data() , input$n)
+  output$preview1 <- renderTable(head(raw(), input$rows))
+#clean --------
+  tidied <- reactive({
+    out <- raw()
+    if (input$snake){
+      name(out) <- janitor::make_clean_names(names(out))
+    }
+    if (input$empty){
+      out <- janitor::remove_empty(out,"cols")
+    }
+    if (input$constant){
+      out <-janitor::remove_constant(out)
+    }
+    out
   })
+  output$preview2 <- renderTable(head(tidied(), input$rows))
+  # download ---------
+  output$download <- downloadHandler(
+    filename = function(){
+      paste0(tools::file_path_sans_ext(input$file$name), ".tsv")
+    },
+    content = function(file) {
+      vroom:vroom_write(tidied(),file)
+    }
+  )
 }
+# upload and parsing file
+ui_upload <- sidebarLayout(
+  sidebarPanel(
+    fileInput("file", "Data", buttonLabel = "Upload..."),
+    textInput("delim", "Delimiter(leave balnk to guess)", ""),
+    numericInput("skip", "Rows to skip", 0, min = 0),
+    numericInput("rows","ows to preview", 10, min = 1)
+  ),
+  mainPanel(
+    h3("Cleaner data"),
+    tableOutput("preview2")
+  )
+)
+#clean file
+ui_clean <- sidebarLayout(
+  sidebarPanel(
+    checkboxInput("snake","Rename col to snake case?"),
+    checkboxInput("constant", "Remove constant columns?"),
+    checkboxInput("empty" , "Remove empty cols?")
+  ),
+  mainPanel(
+    h3("Clener data"),
+    tableOutput("preview2")
+  )
+)
+#downlad file
+ui_download <- fluidRow(
+  column(width = 12 , downloadButton("download" , class = "btn-block"))
+)
 ui <- fluidPage(
-  fileInput("file", NULL , accept = c(".csv" , ".tsv")),
-  numericInput("n" , "Rows" , value = 5,min = 1 , step = 1),
-  tableOutput("head")
+  ui_upload,
+  ui_clean,
+  ui_download
 )
 shinyApp(ui,server)
