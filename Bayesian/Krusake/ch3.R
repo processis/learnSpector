9f0ad59979c5e1a5dae52cef3d0642d66b9b810f
Debian Live user <user@localhost.localdomain>
2025-10-13 16:02:39 +0800

    ch4 ch6

--- Bayesian/Krusake/ch3.R
+++ Bayesian/Krusake/ch3.R
@@ -48,6 +48,12 @@ samples <- coda.samples(model,
 
 summary(samples)
 
+#add
+
+par(mar=c(2,2,1,1))
+
+
+
 plot(samples)
 
 ######################################
@@ -314,3 +320,74 @@ samp  <- coda.samples(model, variable.names=keep, n.iter=2000, progress.bar="non
 plot(samp)
 
 rm(samp,model,data,keep)
+
+
+###Chapter 3.4: Diagnosing and improving convergence
+
+model_string <- textConnection("model{
+   Y     ~ dpois(exp(mu[1]+mu[2]))
+   mu[1] ~ dnorm(0,0.001)
+   mu[2] ~ dnorm(0,0.001)
+ }")
+
+inits <- list(mu=rnorm(2,0,5))
+data  <- list(Y=1)
+model <- jags.model(model_string,data = data, inits=inits, n.chains=3, quiet=TRUE)
+
+update(model, 1000, progress.bar="none")
+samples <- coda.samples(model, 
+                        variable.names=c("mu"), 
+                        n.iter=5000, progress.bar="none")
+#add
+
+par(mar=c(2,2,1,1))
+
+plot(samples)
+
+autocorr.plot(samples)
+
+# Autocorrelation near 1 indicates poor convergence
+autocorr(samples[[1]],lag=1)
+
+# Low ESS indicates poor convergence
+effectiveSize(samples)
+
+# R greater than 1.1 indicates poor convergence
+gelman.diag(samples)
+
+#greater than 2 indicates poor convergence
+geweke.diag(samples[[1]])
+
+
+####Convergence diagnostics for a well-behaved model
+
+model_string <- textConnection("model{
+   Y1    ~ dpois(exp(mu[1]))
+   Y2    ~ dpois(exp(mu[2]))
+   mu[1] ~ dnorm(0,0.001)
+   mu[2] ~ dnorm(0,0.001)
+ }")
+
+inits <- list(mu=rnorm(2,0,5))
+data  <- list(Y1=1,Y2=10)
+model <- jags.model(model_string,data = data, inits=inits, n.chains=3, quiet=TRUE)
+
+update(model, 1000, progress.bar="none")
+samples <- coda.samples(model, 
+                        variable.names=c("mu"), 
+                        n.iter=5000, progress.bar="none")
+
+plot(samples)
+
+autocorr.plot(samples)
+
+# Low autocorrelation indicates convergence
+autocorr(samples[[1]],lag=1)
+
+# ESS over 1000 indicates convergence
+effectiveSize(samples)
+
+# R less than 1.1 indicates convergence
+gelman.diag(samples)
+
+geweke.diag(samples[[1]])
